5620a29cc6bb3c0eec50bb2e142ecfe182cb619a
lalalalalallaajak <pamporijanakajanakanedsk@gmail.com>
2025-04-12 13:36:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "session id",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=I1IjXZTK#rPMm1QA4tOcne-XfTbzwydkyAkNgmIcYs6IJH9-c-RQ",
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
